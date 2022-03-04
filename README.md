b41ccad53b3f5a8118033a5a8e7ae40fbd0ba42c
Algot Sandahl <algot.sandahl@gmail.com>
2022-03-04 09:24:11 +0100

    Some integer scaling for high DPI displays

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ small test of some strategies for avoiding problems with scaling in browsers.
 ## Interesting resources
 - Scaling of images in integer steps with pixel replication | [Demo](https://tanalin.com/_x/demos/integer-scaling/)
   | [Some text](https://tanalin.com/en/articles/integer-scaling/)
-
+- CSS `image-rendering` | [MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/image-rendering)
--- script.js
+++ script.js
@@ -2,11 +2,9 @@ imageScale = 1;
 
 function update() {
   // https://stackoverflow.com/questions/28905420/window-devicepixelratio-change-listener
-  matchMedia(`(resolution: ${window.devicePixelRatio}dppx)`).addEventListener(
-    'change',
-    update,
-    { once: true }
-  );
+  matchMedia(`(resolution: ${window.devicePixelRatio}dppx)`).addEventListener('change', update, {
+    once: true,
+  });
 
   const snailOrigImg = document.getElementById('snail-orig');
   snailOrigImg.style.width =
@@ -16,8 +14,10 @@ function update() {
   infoParagraph.textContent = `devicePixelRatio=${window.devicePixelRatio}`;
 
   const snailScaleImg = document.getElementById('snail-scale');
-  snailScaleImg.style.width = 4 * snailOrigImg.naturalWidth + 'px';
-
+  snailScaleImg.style.width =
+    (Math.round(Math.max(window.devicePixelRatio, 1)) * snailOrigImg.naturalWidth) /
+      window.devicePixelRatio +
+    'px';
 }
 
 update();
